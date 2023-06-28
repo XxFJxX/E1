dee83b8f2f94f593c770df735e5b65927c0722e9
XxFJxX <ferinocente357@gmail.com>
2023-06-27 22:55:09 -0600

    Agregar archivos de proyecto.

---
+++ E1/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace E1;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ E1/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace E1;
+
+public partial class MainPage : ContentPage
+{
+	
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        int nume1 = Convert.ToInt32(num1.Text);
+        int nume2 = Convert.ToInt32(num2.Text);
+        int nume3 = Convert.ToInt32(num3.Text);
+        int nume4 = Convert.ToInt32(num4.Text);
+
+
+        num1.Text = Convert.ToString(nume4);
+        num2.Text = Convert.ToString(nume3);
+        num3.Text = Convert.ToString(nume2);
+        num4.Text = Convert.ToString(nume1);
+    }
+}
+
